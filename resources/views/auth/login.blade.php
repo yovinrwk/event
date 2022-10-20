38a11b30aa5757537f508d71a01d43f66fee4c3b
Giwan Purnama <Giwan24april@gmail.com>
2022-10-20 10:34:11 +0700

    project event ppdb

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <br>
+            <center><img src="https://pbs.twimg.com/profile_images/560621599158591488/SdBbN9Fn_400x400.png" alt="Logo smk pi" width="200px"></center>
+            <br><br>
+            <div class="card"  style="border-radius:20px; border:none; background: rgba(255,255,255,0.8); backdrop-filter: blur(500px); box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.2); ">
+                <div class="card-header" style="border:none;"><center><b>{{ __('Masuk') }}</b></center></div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Kata Sandi') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Ingat saya') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Masuk') }}
+                                </button>
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Lupa Password?') }}
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/index.php
@@ -0,0 +1,365 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link rel="icon" href="https://pbs.twimg.com/profile_images/560621599158591488/SdBbN9Fn_400x400.png">
+	<title>Event PPDB SMK PI</title>
+	<!-- Bootstrap core CSS -->
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Fonts -->
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Righteous" rel="stylesheet">
+	<!-- Custom styles for this template -->
+	<link href="assets/css/mediumish.css" rel="stylesheet">
+</head>
+
+<body>
+
+	<!-- Begin Nav
+================================================== -->
+	<nav class="navbar navbar-toggleable-md navbar-light bg-white fixed-top mediumnavigation">
+		<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+			data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false"
+			aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="container">
+			<img src="https://pbs.twimg.com/profile_images/560621599158591488/SdBbN9Fn_400x400.png" alt="Logo smk pi"
+				width="40px">&nbsp&nbsp&nbsp
+			<a class="navbar-brand" href="index.html">
+				PPDB&nbsp&nbsp|&nbsp&nbspSMK Prakarya Internasional
+			</a>
+			<!-- End Logo -->
+			<div class="collapse navbar-collapse" id="navbarsExampleDefault">
+				<!-- Begin Menu -->
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="index.html"><b>Event</b> <span class="sr-only">(current)</span></a>
+					</li>
+
+				</ul>
+				<!-- End Menu -->
+				<!-- Begin Search -->
+				<form class="form-inline my-2 my-lg-0">
+					<input class="form-control mr-sm-2" type="text" placeholder="Search">
+					<span class="search-icon"><svg class="svgIcon-use" width="25" height="25" viewbox="0 0 25 25">
+							<path
+								d="M20.067 18.933l-4.157-4.157a6 6 0 1 0-.884.884l4.157 4.157a.624.624 0 1 0 .884-.884zM6.5 11c0-2.62 2.13-4.75 4.75-4.75S16 8.38 16 11s-2.13 4.75-4.75 4.75S6.5 13.62 6.5 11z">
+							</path>
+						</svg></span>
+				</form>
+				<!-- End Search -->
+			</div>
+		</div>
+	</nav>
+	<!-- End Nav
+================================================== -->
+
+	<!-- Begin Site Title
+================================================== -->
+	<div class="container">
+		<div class="mainheading">
+			<h1 class="sitetitle">Event PPDB</h1>
+			<p class="lead">
+				Acara event PPDB SMK Prakarya Internasional 2022-2023<br><br>
+				<img
+					src="http://lh4.ggpht.com/Yeo7nNNnJQK9g2SeXA8OSOj-rCK4oakn0VX6ySCMxtSFJWc_N1Zv0SkIMcgeqc5WRxDOG-pRKM2s971E3VDsmDkMXA=s1500"
+					alt="Logo smk pi" style="border-radius: 30px;">
+				<br><br>
+			</p>
+		</div>
+		<!-- End Site Title
+================================================== -->
+
+		<!-- Begin Event
+	================================================== -->
+		<section class="featured-posts">
+			<div class="section-title">
+				<h2><span>Daftar Event</span></h2>
+			</div>
+			<div class="card-columns listfeaturedtag">
+
+				<!-- begin post -->
+				<div class="card" style="border-radius: 15px; box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;">
+					<div class="row">
+						<div class="col-md-5 wrapthumbnail">
+							<a href="post.html">
+								<div class="thumbnail"
+									style="background-image:url(http://lh6.ggpht.com/AgNB7gqrTcasUyRvoixc67FfUYT6jsnepE7oue7XoVzZxgsESlLhQEp2G517kHIq8ajy8Jvf7NtFxt7Vziq6KUaw=s1500); border-top-left-radius: 15px; border-bottom-left-radius: 15px;">
+								</div>
+							</a>
+						</div>
+						<div class="col-md-7">
+							<div class="card-block">
+								<h2 class="card-title"><a href="post.html">Kegiatan MPLS 2022</a></h2>
+								<h4 class="card-text">This is a longer card with supporting text below as a natural lead-in to
+									additional content. This content is a little bit longer.</h4>
+								<div class="metafooter">
+									<div class="wrapfooter">
+										<span class="meta-footer-thumb">
+											<a href="author.html"><img class="author-thumb"
+													src="https://lh3.googleusercontent.com/uWfqJ0OrE8kSBFZq36x940-zQXSb0NSgGQKp0hJEnyS2S32v61U9WMoWd_xOyzfTDvrNLS41W8dP9MIoTg=s180"
+													alt="Sal"></a>
+										</span>
+										<span class="author-meta">
+											<span class="post-name"><a href="author.html">SMK PI</a></span><br />
+											<span class="post-date" style="color: rgb(5, 161, 57);"> Tanggal : 10 July 2022</span>
+										</span>
+										<span class="post-read-more"><a href="post.html" title="Read Story"><svg class="svgIcon-use"
+													width="25" height="25" viewbox="0 0 25 25">
+													<path
+														d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z"
+														fill-rule="evenodd"></path>
+												</svg></a></span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- end post -->
+
+				<!-- begin post -->
+				<div class="card" style="border-radius: 15px; box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;">
+					<div class="row">
+						<div class="col-md-5 wrapthumbnail">
+							<a href="post.html">
+								<div class="thumbnail"
+									style="background-image:url(https://jabarekspres.com/wp-content/uploads/2017/05/SMK-Prakarya-Internasional-Gelar-Wisuda-1-.jpg); border-top-left-radius: 15px; border-bottom-left-radius: 15px;">
+								</div>
+							</a>
+						</div>
+						<div class="col-md-7">
+							<div class="card-block">
+								<h2 class="card-title"><a href="post.html">Pelepasan Alumni 2022</a></h2>
+								<h4 class="card-text">This is a longer card with supporting text below as a natural lead-in to
+									additional content. This content is a little bit longer.</h4>
+								<div class="metafooter">
+									<div class="wrapfooter">
+										<span class="meta-footer-thumb">
+											<a href="author.html"><img class="author-thumb"
+													src="https://lh3.googleusercontent.com/uWfqJ0OrE8kSBFZq36x940-zQXSb0NSgGQKp0hJEnyS2S32v61U9WMoWd_xOyzfTDvrNLS41W8dP9MIoTg=s180"
+													alt="Sal"></a>
+										</span>
+										<span class="author-meta">
+											<span class="post-name"><a href="author.html">SMK PI</a></span><br />
+											<span class="post-date" style="color: rgb(5, 161, 57);"> Tanggal : 10 July 2022</span>
+										</span>
+										<span class="post-read-more"><a href="post.html" title="Read Story"><svg class="svgIcon-use"
+													width="25" height="25" viewbox="0 0 25 25">
+													<path
+														d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z"
+														fill-rule="evenodd"></path>
+												</svg></a></span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- end post -->
+
+				<!-- begin post -->
+				<div class="card" style="border-radius: 15px; box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;">
+					<div class="row">
+						<div class="col-md-5 wrapthumbnail">
+							<a href="post.html">
+								<div class="thumbnail"
+									style="background-image:url(https://media-exp1.licdn.com/dms/image/C5605AQFgO5o6gZJS3w/videocover-low/0/1641125952229?e=2147483647&v=beta&t=gCJYgbV4VY03AZqYoRUNnuZmL6Ws53Y-dXq5MUf1pDg); border-top-left-radius: 15px; border-bottom-left-radius: 15px;">
+								</div>
+							</a>
+						</div>
+						<div class="col-md-7">
+							<div class="card-block">
+								<h2 class="card-title"><a href="post.html">Rencana alur pendaftaran PPDB Tahun 2023</a></h2>
+								<h4 class="card-text">This is a longer card with supporting text below as a natural lead-in to
+									additional content. This content is a little bit longer.</h4>
+								<div class="metafooter">
+									<div class="wrapfooter">
+										<span class="meta-footer-thumb">
+											<a href="author.html"><img class="author-thumb"
+													src="https://lh3.googleusercontent.com/uWfqJ0OrE8kSBFZq36x940-zQXSb0NSgGQKp0hJEnyS2S32v61U9WMoWd_xOyzfTDvrNLS41W8dP9MIoTg=s180"
+													alt="Sal"></a>
+										</span>
+										<span class="author-meta">
+											<span class="post-name"><a href="author.html">SMK PI</a></span><br />
+											<span class="post-date" style="color: rgb(5, 161, 57);"> Tanggal : 10 July 2022</span>
+										</span>
+										<span class="post-read-more"><a href="post.html" title="Read Story"><svg class="svgIcon-use"
+													width="25" height="25" viewbox="0 0 25 25">
+													<path
+														d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z"
+														fill-rule="evenodd"></path>
+												</svg></a></span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- end post -->
+
+				<!-- begin post -->
+				<div class="card" style="border-radius: 15px; box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;">
+					<div class="row">
+						<div class="col-md-5 wrapthumbnail">
+							<a href="post.html">
+								<div class="thumbnail"
+									style="background-image:url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNn0n0iQ63rqwEro0Ljrad5swiLpehphq70Q&usqp=CAU); border-top-left-radius: 15px; border-bottom-left-radius: 15px;">
+								</div>
+							</a>
+						</div>
+						<div class="col-md-7">
+							<div class="card-block">
+								<h2 class="card-title"><a href="post.html">Pengenalan jurusan SMK PI</a></h2>
+								<h4 class="card-text">This is a longer card with supporting text below as a natural lead-in to
+									additional content. This content is a little bit longer.</h4>
+								<div class="metafooter">
+									<div class="wrapfooter">
+										<span class="meta-footer-thumb">
+											<a href="author.html"><img class="author-thumb"
+													src="https://lh3.googleusercontent.com/uWfqJ0OrE8kSBFZq36x940-zQXSb0NSgGQKp0hJEnyS2S32v61U9WMoWd_xOyzfTDvrNLS41W8dP9MIoTg=s180"
+													alt="Sal"></a>
+										</span>
+										<span class="author-meta">
+											<span class="post-name"><a href="author.html">SMK PI</a></span><br />
+											<span class="post-date" style="color: rgb(5, 161, 57);"> Tanggal : 10 July 2022</span>
+										</span>
+										<span class="post-read-more"><a href="post.html" title="Read Story"><svg class="svgIcon-use"
+													width="25" height="25" viewbox="0 0 25 25">
+													<path
+														d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z"
+														fill-rule="evenodd"></path>
+												</svg></a></span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- end post -->
+
+				<!-- begin post -->
+				<div class="card" style="border-radius: 15px; box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;">
+					<div class="row">
+						<div class="col-md-5 wrapthumbnail">
+							<a href="post.html">
+								<div class="thumbnail"
+									style="background-image:url(http://lh6.ggpht.com/rPxMkPB7wGDl3pST4AVWljHepzKVxLphntMP9OM_QrhKcDivN_uFdVmHcsnKp5Fgr2HEpbyxlbmVaNI2To0EJOitvA=s391); border-top-left-radius: 15px; border-bottom-left-radius: 15px;">
+								</div>
+							</a>
+						</div>
+						<div class="col-md-7">
+							<div class="card-block">
+								<h2 class="card-title"><a href="post.html">Kegiatan MPLS 2023</a></h2>
+								<h4 class="card-text">This is a longer card with supporting text below as a natural lead-in to
+									additional content. This content is a little bit longer.</h4>
+								<div class="metafooter">
+									<div class="wrapfooter">
+										<span class="meta-footer-thumb">
+											<a href="author.html"><img class="author-thumb"
+													src="https://lh3.googleusercontent.com/uWfqJ0OrE8kSBFZq36x940-zQXSb0NSgGQKp0hJEnyS2S32v61U9WMoWd_xOyzfTDvrNLS41W8dP9MIoTg=s180"
+													alt="Sal"></a>
+										</span>
+										<span class="author-meta">
+											<span class="post-name"><a href="author.html">SMK PI</a></span><br />
+											<span class="post-date" style="color: rgb(5, 161, 57);"> Tanggal : 10 July 2022</span>
+										</span>
+										<span class="post-read-more"><a href="post.html" title="Read Story"><svg class="svgIcon-use"
+													width="25" height="25" viewbox="0 0 25 25">
+													<path
+														d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z"
+														fill-rule="evenodd"></path>
+												</svg></a></span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- end post -->
+
+				<!-- begin post -->
+				<div class="card" style="border-radius: 15px; box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;">
+					<div class="row">
+						<div class="col-md-5 wrapthumbnail">
+							<a href="post.html">
+								<div class="thumbnail"
+									style="background-image:url(http://lh6.ggpht.com/AgNB7gqrTcasUyRvoixc67FfUYT6jsnepE7oue7XoVzZxgsESlLhQEp2G517kHIq8ajy8Jvf7NtFxt7Vziq6KUaw=s1500); border-top-left-radius: 15px; border-bottom-left-radius: 15px;">
+								</div>
+							</a>
+						</div>
+						<div class="col-md-7">
+							<div class="card-block">
+								<h2 class="card-title"><a href="post.html">Kegiatan MPLS 2022</a></h2>
+								<h4 class="card-text">This is a longer card with supporting text below as a natural lead-in to
+									additional content. This content is a little bit longer.</h4>
+								<div class="metafooter">
+									<div class="wrapfooter">
+										<span class="meta-footer-thumb">
+											<a href="author.html"><img class="author-thumb"
+													src="https://lh3.googleusercontent.com/uWfqJ0OrE8kSBFZq36x940-zQXSb0NSgGQKp0hJEnyS2S32v61U9WMoWd_xOyzfTDvrNLS41W8dP9MIoTg=s180"
+													alt="Sal"></a>
+										</span>
+										<span class="author-meta">
+											<span class="post-name"><a href="author.html">SMK PI</a></span><br />
+											<span class="post-date" style="color: rgb(5, 161, 57);"> Tanggal : 10 July 2022</span>
+										</span>
+										<span class="post-read-more"><a href="post.html" title="Read Story"><svg class="svgIcon-use"
+													width="25" height="25" viewbox="0 0 25 25">
+													<path
+														d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z"
+														fill-rule="evenodd"></path>
+												</svg></a></span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- end post -->
+
+			</div>
+		</section>
+		<!-- End Event
+	================================================== -->
+
+
+
+		<!-- Begin Footer
+	================================================== -->
+		<div class="footer">
+			<p class="pull-left">
+				Copyright &copy; 2022 Klinik Coding
+			</p>
+			<p class="pull-right">
+				PPDB by <a target="_blank" href="http://www.smk-pi.sch.id/"
+					style="text-decoration: none; color: rgb(167, 167, 255);">SMK
+					Prakarya
+					Internasional</a>
+			</p>
+			<div class="clearfix">
+			</div>
+		</div>
+		<!-- End Footer
+	================================================== -->
+
+	</div>
+	<!-- /.container -->
+
+	<!-- Bootstrap core JavaScript
+    ================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"
+		integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb"
+		crossorigin="anonymous"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
+
+</html>
